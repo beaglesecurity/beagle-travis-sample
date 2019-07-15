4b924dfedd481edb07f87560793ea6824f535b3a
Anees <anees.pk@appfabs.com>
2019-07-15 17:24:27 +0530

    Script issues fixed

--- beagle.sh
+++ beagle.sh
@@ -1,6 +1,8 @@
-URL="https://beagle-tvm-api.appfabs.com/v1/test/start/"
-DATA= '{"user_token":"${user_token}","application_token":"${application_token}"}'
-curl --header "Content-Type: application/json" \
-	--request POST \
-	--data $DATA \
-	$URL
+URL=https://beagle-tvm-api.appfabs.com/v1/test/start/
+curl --header "Content-Type: application/json" --request POST --data "{\"user_token\":\"$user_token\", \"application_token\":\"$application_token\"}"  $URL
+
+if test $? -eq 0;  then 
+	echo "success!"
+else
+	echo "Error"
+fi
