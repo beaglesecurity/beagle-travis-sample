77ec3677eb2546902d8a2d4fb721e8f2025560ce
Anees <anees.pk@appfabs.com>
2019-07-15 15:42:01 +0530

    Typo fix

--- beagle.sh
+++ beagle.sh
@@ -1,4 +1,6 @@
 URL="https://beagle-tvm-api.appfabs.com/v1/test/start/"
+DATA= '{"user_token":"${user_token}","application_token":"${application_token}"}'
 curl --header "Content-Type: application/json" \
 	--request POST \
-	--data '{"user_token":"${user_token}","application_token":"${application_token}"' ${URL}
+	--data $DATA \
+	$URL
