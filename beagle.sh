49d422485a84299873cc6eb7b4178b29ff3d266a
Anees <anees.pk@appfabs.com>
2019-07-15 20:29:11 +0530

    Script update

--- beagle.sh
+++ beagle.sh
@@ -1,2 +1,3 @@
 URL=https://beagle-tvm-api.appfabs.com/v1/test/start/
-curl --silent --header "Content-Type: application/json" --request POST --data "{\"user_token\":\"$user_token\", \"application_token\":\"$application_token\"}"  $URL | cut -d '"' -f 4,8 | awk '{split($0,a,"\""); print "Status: ",a[1]}{split($0,a,"\""); print "Message: ",a[2]}'
+curl --silent --header "Content-Type: application/json" --request POST --data "{\"user_token\":\"$user_token\",\"application_token\":\"$application_token\"}" $URL | \
+    sed -e 's/[{}]/''/g' | awk 'BEGIN { FS="\""; RS="," }; { if ($2 == "status") { print "Status : "$4} if ($2 == "message") { print "Message : "$4} }'
