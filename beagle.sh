d46a4b0fa96f0331129ab67f1ea349214f6656c4
Anees <anees.pk@appfabs.com>
2019-07-15 18:59:21 +0530

    Output formatted

--- beagle.sh
+++ beagle.sh
@@ -1,8 +1,2 @@
 URL=https://beagle-tvm-api.appfabs.com/v1/test/start/
-curl --header "Content-Type: application/json" --request POST --data "{\"user_token\":\"$user_token\", \"application_token\":\"$application_token\"}"  $URL
-
-if test $? -eq 0;  then 
-	echo "success!"
-else
-	echo "Error"
-fi
+curl --silent --header "Content-Type: application/json" --request POST --data "{\"user_token\":\"$user_token\", \"application_token\":\"$application_token\"}"  $URL | cut -d '"' -f 4,8 | awk '{split($0,a,"\""); print "Status: ",a[1]}{split($0,a,"\""); print "Message: ",a[2]}'
